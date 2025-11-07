ddace09f1dc53cee8ee9268e17cb9561cdd73284
Nipun Jindal <mansapatiala@gmail.com>
2025-11-07 12:58:26 +0530

    setting up and connecting to mongodb

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyparser from "body-parser";
 import cookieparser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
 
 // Initialize APP
 const app = express();
@@ -24,3 +25,32 @@ const server = http.createServer(app);
 server.listen(8080, ()=>{
     console.log("Server running on http://localhost:8080")
 });
+
+// mongodb+srv://<username>:<password>@<host_url>:><port>?options...
+const MONGO_URL = "mongodb://admin:password@localhost:27017?retryWrites=true";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error: Error) => {
+    console.error("MongoDB connection error:", error);
+});
+
+const gracefulShutdown = (signal: string) => {
+    console.log(`Received ${signal}. Closing http server.`);
+    server.close(async () => {
+        console.log('Http server closed.');
+        await mongoose.connection.close(false);
+        console.log('MongoDB connection closed.');
+        process.exit(0);
+    });
+};
+
+// For nodemon restarts
+process.once('SIGUSR2', () => gracefulShutdown('SIGUSR2'));
+
+// For app termination
+process.on('SIGINT', () => gracefulShutdown('SIGINT'));
+
+// For container termination
+process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
